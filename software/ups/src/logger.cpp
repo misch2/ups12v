e77623bab7afd04188a6a007c67d0f0172badaba
Michal Schwarz <michal.schwarz@gmail.com>
2025-07-02 14:46:36 +0200

    fix log to serial

--- software/ups/src/logger.cpp
+++ software/ups/src/logger.cpp
@@ -2,6 +2,8 @@
 
 #include <WiFi.h>
 
+#include <cstdarg>
+
 Logger::Logger(Syslog* syslog, Stream* serial) {
   this->syslog = syslog;
   this->serial = serial;
@@ -10,12 +12,15 @@ Logger::Logger(Syslog* syslog, Stream* serial) {
 void Logger::log(uint16_t pri, const char* fmt, ...) {
   va_list args;
   va_start(args, fmt);
+
   if (syslog && WiFi.status() == WL_CONNECTED) {
     syslog->vlogf(pri, fmt, args);
   }
+
   if (serial) {
-    serial->printf(fmt, args);
-    serial->println();
+    char buffer[256];
+    vsnprintf(buffer, sizeof(buffer), fmt, args);
+    serial->println(buffer);  // can't used printf() here because it doesn't support va_args!
     serial->flush();
   }
   va_end(args);
